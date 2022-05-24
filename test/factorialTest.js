51254665c13b3dda850ef5102952612776517d02
Brisa-Codes <brisamukunde1@gmail.com>
2022-05-24 08:09:40 +0300

    update: factorial comments

--- test/factorialTest.js
+++ test/factorialTest.js
@@ -7,15 +7,18 @@ const multiply = require('../factorial');
 
 describe('Testing our Factorial', () => {
   // first factorial test
+  // 4 - 4 * 3 * 2 * 1
   it('should factorilize 4 and return 24', () => {
     assert.equal(factorialize(4), 24);
   });
     // second factorial test
+    // 5 - 5 * 4 * 3 * 2 * 1
     it('should factorilize 5 and return 120', () => {
         assert.equal(factorialize(5), 120);
     });
 
     // third factorial test
+    // 6 - 6 * 5 * 4 * 3 * 2 * 1
     it('should factorilize 6 and return 720', () => {
         assert.equal(factorialize(6), 720);
     });
